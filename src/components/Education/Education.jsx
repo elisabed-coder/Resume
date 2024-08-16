58a4efae017bbb60232627552040e86ac1d38922
eli <ebezhiasvhili@gmail.com>
2024-08-16 14:24:50 -0700

    add education styles

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,3 +1,48 @@
+// import React from "react";
+// import "./education.css";
+// import { motion } from "framer-motion";
+// import Educationdata from "./Educationdata";
+
+// const Education = () => {
+//   return (
+//     <section className="section education" id="education">
+//       <motion.h1
+//         initial={{ opacity: 0, y: -50 }}
+//         animate={{ opacity: 1, y: 0 }}
+//         transition={{ duration: 0.5 }}
+//         className="section__title"
+//       >
+//         Education
+//       </motion.h1>
+//       <div className="education__container container">
+//         {Educationdata.map((education, index) => {
+//           return (
+//             <div key={index} className="education__tabs">
+//               <div className="education__year">
+//                 <p>
+//                   <i className="uil uil-schedule"></i>
+//                   {education.year}
+//                 </p>
+//               </div>
+//               <div className="education__timeline">
+//                 <span className="rounder"></span>
+//                 <span className="education__line"></span>
+//               </div>
+//               <div className="education__content">
+//                 <h3>{education.faculty}</h3>
+//                 <p>{education.university}</p>
+//                 <p>{education.description}</p>
+//               </div>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Education;
+
 import React from "react";
 import "./education.css";
 import { motion } from "framer-motion";
@@ -5,7 +50,7 @@ import Educationdata from "./Educationdata";
 
 const Education = () => {
   return (
-    <section className="section education" id="education">
+    <section className="section timeline" id="education">
       <motion.h1
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
@@ -14,17 +59,25 @@ const Education = () => {
       >
         Education
       </motion.h1>
-      <div>
+      <div className="timeline__items container">
         {Educationdata.map((education, index) => {
           return (
-            <div key={index} className="education__container">
-              <div>
-                <p>{education.year}</p>
-              </div>
-              <div>
-                <h3>{education.faculty}</h3>
-                <p>{education.university}</p>
-                <p>{education.description}</p>
+            <div key={index} className="timeline__container">
+              <div className="timeline__item">
+                <div className="timeline__dot"></div>
+                <div className="timeline__date">
+                  {" "}
+                  <p>
+                    <i className="uil uil-schedule"></i>
+                    {education.year}
+                  </p>
+                </div>
+                <div className="timeline__content">
+                  {" "}
+                  <h3>{education.faculty}</h3>
+                  <p>{education.university}</p>
+                  <p>{education.description}</p>
+                </div>
               </div>
             </div>
           );
